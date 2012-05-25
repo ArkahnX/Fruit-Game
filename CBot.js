ad302a7072c9b4ce4385d24ed94e7d868a590ab0
Christopher Hall <biomech.arkahnx@gmail.com>
2012-05-25 00:48:54 -0700

    using JBot's code coutesy of Jason
    With Jason's hard work I was able to detect a simple way to win over the
    AI, which is to collect the lesser number items first before collecting
    everything else

--- CBot.js
+++ CBot.js
@@ -1,21 +1,6 @@
 function new_game() {
-	items = itemLocations();
 }
-var previousGrabbedItems = 0;
 var items = [];
-function itemTaken() {
-	var itemTypes = get_number_of_item_types();
-	var grabbedItems = 0;
-	for(var i=0;i<itemTypes;i++) {
-		grabbedItems = grabbedItems+get_my_item_count(i+1);
-		grabbedItems = grabbedItems+get_opponent_item_count(i+1);
-	}
-	if(grabbedItems > previousGrabbedItems) {
-		previousGrabbedItems = grabbedItems;
-		return true;
-	}
-	return false;
-}
 function itemLocations() {
 	var locationsArray = [];
 	var board = get_board();
@@ -23,8 +8,17 @@ function itemLocations() {
 	var ylength = board[0].length;
 	for(var i=0;i<xlength;i++){
 		for(var q=0;q<ylength;q++){
-			if (board[i][q] != 0) {
+			if (board[i][q] === 1 || board[i][q] === 2) {
 				locationsArray.push([i,q]);
+			}
+		}
+	}
+	if(locationsArray.length === 0) {
+		for(var i=0;i<xlength;i++){
+			for(var q=0;q<ylength;q++){
+				if (board[i][q] !== 0) {
+					locationsArray.push([i,q]);
+				}
 			}
 		}
 	}
@@ -32,36 +26,17 @@ function itemLocations() {
 }
 function make_move() {
 	var board = get_board();
-	if(itemTaken()) {
-		items = itemLocations();
-	}
+	items = itemLocations();
 	// we found an item! take it!
 	if (board[get_my_x()][get_my_y()] > 0) {
 		return TAKE;
 	}
-	// Searching for a target fruit
-	for(var i=0;i<xlength;i++){
-		for(var q=0;q<ylength;q++){
-			// Checks each space for a fruit
-			if (board[i][q] != 0) {
-				// Finds the x and y distances from the robot
-				distx = Math.abs(i-get_my_x());
-				disty = Math.abs(q-get_my_y());
-				// Finds the direct distance from the robot
-				distz = Math.sqrt(distx*distx + disty+ disty);
-				// Saves only the SMALLEST distance
-				if (distz < minz) {
-					minz = distz;
-					// Saves the coordinates of the closest one
-					minx = i;
-					miny = q;
-				}
-			}
-		}
-	}
+	var index = min();
+
+
 	// Takes the resulting one
-	targetx = minx;
-	targety = miny;
+	var targetx = index[0];
+	var targety = index[1];
 	// Movement
 	if (get_my_y() > targety) return NORTH;
 	if (get_my_y() < targety) return SOUTH;
@@ -70,13 +45,16 @@ function make_move() {
 	return PASS;
 }
 function min() {
-	var min = (get_my_x()-items[0][0])+(get_my_y()-items[0][1]);
+	var min = Math.abs(items[0][0]-get_my_x())+Math.abs(items[0][1]-get_my_y());
 	var minIndex = 0;
 	for(var i=1;i<items.length;i++) {
-		var newMin = (get_my_x()-items[0][0])+(get_my_y()-items[0][1]);
+		var newMin = Math.abs(items[i][0]-get_my_x())+Math.abs(items[i][1]-get_my_y());
+		// console.log(min, newMin)
 		if(newMin < min) {
 			min = newMin;
 			minIndex = i;
 		}
 	}
+	// console.log(items,items[minIndex])
+	return items[minIndex];
 }
