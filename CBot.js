d855ef3f36df96cc9e104becfa298a1eed259088
Christopher Hall <biomech.arkahnx@gmail.com>
2012-05-25 09:21:38 -0700

    Improved algorithm and bugfixes
    wrapped all code in "isFruit" function to fix errors when trying to
    start a game after a game has been completed

--- CBot.js
+++ CBot.js
@@ -1,59 +1,91 @@
-function new_game() {
-	items = itemLocations();
-	shortcut = true;
-}
-var items = [];
+var items;
+var shortcutItems;
 var shortcut = false;
+function new_game() {
+	if(fruitLeft()) {
+		console.log("started")
+		var shortcut = false;
+		var itemAmounts = [];
+		var itemTypes = get_number_of_item_types();
+		for(var i=0;i<itemTypes;i++) {
+			itemAmounts.push(get_total_item_count(i+1));
+		}
+		var sorted = itemAmounts.sort();
+		shortcutItems = [];
+		for(var i=0;i<Math.ceil(itemTypes/2);i++) {
+			shortcutItems.push(itemAmounts.indexOf(sorted[i])+1);
+		}
+		items = itemLocations();
+	}
+}
+function fruitLeft() {
+	var itemTypes = get_number_of_item_types();
+	var itemAmounts = 0;
+	for(var i=0;i<itemTypes;i++) {
+		itemAmounts = itemAmounts+get_total_item_count(i+1);
+	}
+	return itemAmounts;
+}
+
 function itemLocations() {
-	var locationsArray = [];
-	var board = get_board();
-	var xlength = board.length;
-	var ylength = board[0].length;
-	for(var i=0;i<xlength;i++){
-		for(var q=0;q<ylength;q++){
-			if(shortcut && locationsArray.length === 0) {
-				if (board[i][q] !== 0) {
-					locationsArray.push([i,q]);
-				}
-			} else {
-				if (board[i][q] === 1 || board[i][q] === 2) {
-					locationsArray.push([i,q]);
+	if(fruitLeft()) {
+		// console.log("running")
+		var locationsArray = [];
+		var board = get_board();
+		var xlength = board.length;
+		var ylength = board[0].length;
+		for(var i=0;i<xlength;i++){
+			for(var q=0;q<ylength;q++){
+				if(shortcut) {
+					if (board[i][q] !== 0) {
+						locationsArray.push([i,q]);
+					}
+				} else {
+					if (shortcutItems.indexOf(board[i][q]) > -1) {
+						locationsArray.push([i,q]);
+					}
 				}
 			}
 		}
+		if(locationsArray.length === 0) {
+			shortcut = true;
+			return itemLocations();
+		}
+		return locationsArray;
 	}
-	return locationsArray;
 }
 function make_move() {
-	var board = get_board();
-	items = itemLocations();
-	// we found an item! take it!
-	if (board[get_my_x()][get_my_y()] > 0) {
-		return TAKE;
+	if(fruitLeft()) {
+		var board = get_board();
+		// we found an item! take it!
+		var index = min();
+		if (board[get_my_x()][get_my_y()] > 0) {
+			return TAKE;
+		}
+		// Takes the resulting one
+		var targetx = index[0];
+		var targety = index[1];
+		// Movement
+		if (get_my_y() > targety) return NORTH;
+		if (get_my_y() < targety) return SOUTH;
+		if (get_my_x() < targetx) return EAST;
+		if (get_my_x() > targetx) return WEST;
+		return PASS;
 	}
-	var index = min();
-
-	// Takes the resulting one
-	var targetx = index[0];
-	var targety = index[1];
-	// Movement
-	if (get_my_y() > targety) return NORTH;
-	if (get_my_y() < targety) return SOUTH;
-	if (get_my_x() < targetx) return EAST;
-	if (get_my_x() > targetx) return WEST;
-	return PASS;
 }
 function min() {
-	var min = Math.abs(items[0][0]-get_my_x())+Math.abs(items[0][1]-get_my_y());
-	var minIndex = 0;
-	for(var i=1;i<items.length;i++) {
-		var newMin = Math.abs(items[i][0]-get_my_x())+Math.abs(items[i][1]-get_my_y());
-		// console.log(min, newMin)
-		if(newMin < min) {
-			min = newMin;
-			minIndex = i;
+	if(fruitLeft()) {
+		items = itemLocations();
+		console.log(items)
+		var min = Math.abs(items[0][0]-get_my_x())+Math.abs(items[0][1]-get_my_y());
+		var minIndex = 0;
+		for(var i=1;i<items.length;i++) {
+			var newMin = Math.abs(items[i][0]-get_my_x())+Math.abs(items[i][1]-get_my_y());
+			if(newMin < min) {
+				min = newMin;
+				minIndex = i;
+			}
 		}
+		return items[minIndex];
 	}
-	// console.log(items,items[minIndex])
-	return items[minIndex];
 }
