c1d8ff8fa1e9e2279d878394ac823fef1dcbda56
Christopher Hall <biomech.arkahnx@gmail.com>
2012-05-25 12:00:19 -0700

    Version 3 of CBot

---
+++ HKMk3.js
@@ -0,0 +1,90 @@
+// http://www.scribd.com/jobs/botrace_bot/166
+var items;
+var shortcutItems;
+var shortcut = false;
+
+function new_game() {
+	if (fruitLeft()) {
+		shortcut = false;
+		var itemAmounts = [];
+		var itemTypes = get_number_of_item_types();
+		for (var i = 0; i < itemTypes; i++) {
+			itemAmounts.push(get_total_item_count(i + 1));
+		}
+		var sorted = itemAmounts.sort();
+		shortcutItems = [];
+		for (var i = 0; i < Math.ceil(itemTypes / 2); i++) {
+			shortcutItems.push(itemAmounts.indexOf(sorted[i]) + 1);
+		}
+		items = itemLocations();
+	}
+}
+
+function fruitLeft() {
+	var itemTypes = get_number_of_item_types();
+	var itemAmounts = 0;
+	for (var i = 0; i < itemTypes; i++) {
+		itemAmounts = itemAmounts + get_total_item_count(i + 1);
+	}
+	return itemAmounts;
+}
+
+function itemLocations() {
+	var locationsArray = [];
+	var board = get_board();
+	var xlength = board.length;
+	var ylength = board[0].length;
+	for (var i = 0; i < xlength; i++) {
+		for (var q = 0; q < ylength; q++) {
+			if (board[i][q] > 0) {
+				if (!locationsArray[board[i][q] - 1]) {
+					locationsArray[board[i][q] - 1] = [];
+				}
+				locationsArray[board[i][q] - 1].push([i, q]);
+			}
+		}
+	}
+	var newLocations = [];
+	for (var i = 0; i < locationsArray.length; i++) {
+		if (locationsArray[i]) {
+			newLocations.push(locationsArray[i]);
+		}
+	}
+	return newLocations;
+}
+
+function make_move() {
+	if (fruitLeft()) {
+		var board = get_board();
+		var index = min();
+		var targetx = index[0];
+		var targety = index[1];
+		// we found an item! take it!
+		if (board[get_my_x()][get_my_y()] > 0) {
+			return TAKE;
+		}
+		if (get_my_x() === targetx && get_my_y() === targety) {
+			return TAKE;
+		}
+		// Movement
+		if (get_my_y() > targety) return NORTH;
+		if (get_my_y() < targety) return SOUTH;
+		if (get_my_x() < targetx) return EAST;
+		if (get_my_x() > targetx) return WEST;
+		return PASS;
+	}
+}
+
+function min() {
+	items = itemLocations();
+	var min = Math.abs(items[0][0][0] - get_my_x()) + Math.abs(items[0][0][1] - get_my_y());
+	var minIndex = 0;
+	for (var i = 1; i < items[0].length; i++) {
+		var newMin = Math.abs(items[0][i][0] - get_my_x()) + Math.abs(items[0][i][1] - get_my_y());
+		if (newMin < min) {
+			min = newMin;
+			minIndex = i;
+		}
+	}
+	return items[0][minIndex];
+}
